d5b3253200911bf1e901c289dec5bd865f92d4eb
evankozliner <evankozliner@gmail.com>
2018-04-28 11:18:44 -0700

    Quick adjustment to record probabilities for ROC curve

--- tensorflow/examples/label_image/label_image.py
+++ tensorflow/examples/label_image/label_image.py
@@ -20,8 +20,9 @@ from __future__ import print_function
 import argparse
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
-
+import csv
 
 def load_graph(model_file):
   graph = tf.Graph()
@@ -86,6 +87,7 @@ if __name__ == "__main__":
 
   parser = argparse.ArgumentParser()
   parser.add_argument("--image", help="image to be processed")
+  parser.add_argument("--image_dir", help="Directory of images. Requires --test_data_location")
   parser.add_argument("--graph", help="graph/model to be executed")
   parser.add_argument("--labels", help="name of file containing labels")
   parser.add_argument("--input_height", type=int, help="input height")
@@ -94,6 +96,7 @@ if __name__ == "__main__":
   parser.add_argument("--input_std", type=int, help="input std")
   parser.add_argument("--input_layer", help="name of input layer")
   parser.add_argument("--output_layer", help="name of output layer")
+  parser.add_argument("--test_data_location", help="test data with column 'id' and column 'benign_malignant'")
   args = parser.parse_args()
 
   if args.graph:
@@ -112,29 +115,73 @@ if __name__ == "__main__":
     input_std = args.input_std
   if args.input_layer:
     input_layer = args.input_layer
+  # test_data_download_format.csv for example
+  if args.test_data_location:
+    test_data = args.test_data_location
+  if args.image_dir:
+    image_dir = args.image_dir
   if args.output_layer:
     output_layer = args.output_layer
 
   graph = load_graph(model_file)
-  t = read_tensor_from_image_file(
-      file_name,
-      input_height=input_height,
-      input_width=input_width,
-      input_mean=input_mean,
-      input_std=input_std)
 
+  # Get the test set
+
+  # Read all test set into tensors
   input_name = "import/" + input_layer
   output_name = "import/" + output_layer
   input_operation = graph.get_operation_by_name(input_name)
   output_operation = graph.get_operation_by_name(output_name)
 
-  with tf.Session(graph=graph) as sess:
-    results = sess.run(output_operation.outputs[0], {
-        input_operation.outputs[0]: t
-    })
-  results = np.squeeze(results)
 
-  top_k = results.argsort()[-5:][::-1]
-  labels = load_labels(label_file)
-  for i in top_k:
-    print(labels[i], results[i])
+  with tf.Session(graph=graph) as sess:
+    # for each ro win the test set
+    if args.test_data_location:
+      roc_info = open('roc_info.csv', 'w+')
+      writer = csv.writer(roc_info)
+      writer.writerow([label + "_prob" for label in load_labels(label_file)] + ['benign_malignant'])
+
+      test = pd.read_csv(args.test_data_location)
+      # run the session. Filenames are the same as image ids in my case.
+      tensors = [read_tensor_from_image_file(args.image_dir + "/" + file,
+                                             input_height = input_height,
+                                             input_width = input_width,
+                                             input_mean = input_mean,
+                                             input_std = input_std)
+                 for file in test['id']]
+
+      for i,tensor in enumerate(tensors):
+        # TODO extract to function
+        results = sess.run(output_operation.outputs[0], {
+          input_operation.outputs[0]: tensor
+        })
+        results = np.squeeze(results)
+        writer.writerow([prob for prob in results] + [test.iloc[i]['benign_malignant']])
+
+        top_k = results.argsort()[-5:][::-1]
+        labels = load_labels(label_file)
+        for i in top_k:
+          print(labels[i], results[i])
+
+      roc_info.close()
+    else:
+      t = read_tensor_from_image_file(
+        file_name,
+        input_height=input_height,
+        input_width=input_width,
+        input_mean=input_mean,
+        input_std=input_std)
+
+      results = sess.run(output_operation.outputs[0], {
+        input_operation.outputs[0]: t
+      })
+  if args.test_data_location:
+    print
+    # do the thing
+  else:
+    results = np.squeeze(results)
+
+    top_k = results.argsort()[-5:][::-1]
+    labels = load_labels(label_file)
+    for i in top_k:
+      print(labels[i], results[i])
